2d14ca4ff02e97a25052c9cfd47e804be845334d
Roshan Acharya <acharyaroshan2357@gmail.com>
2026-05-02 20:44:06 +0545

    feat: basic shadcn tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { assemble } from "./core/assembler";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "./components/theme-toggle";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log(
-    assemble(`
-      LDA 2000H
-    `),
-  );
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
+      <div className="flex">
+        <div className="flex justify-end w-full">
+          <ThemeToggle />
+        </div>
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
   );
 }
 
